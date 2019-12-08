54cbe97f2c6f43ec42bb9d76bc958105d1e83523
root <root@localhost.localdomain>
2019-12-08 11:21:58 +0100

    doc_pod rights x

---
+++ idea_001.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo wget https://download-cf.jetbrains.com/idea/ideaIC-2018.3.2.tar.gz
+tar -xvf ideaIC-2018.3.2.tar.gz
+
+cd ideaIC-2018.3.2
+
+sudo ln -s ./ideaIC-2018.3.2/bin/idea.sh /usr/bin/idea
+
+
