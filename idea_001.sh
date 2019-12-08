3a9b3f51dd4a4ba1d45c19bedce246a9386fa5f0
root <root@localhost.localdomain>
2019-12-08 18:31:32 +0100

    Idea minor change

--- idea_001.sh
+++ idea_001.sh
@@ -3,7 +3,7 @@
 sudo wget https://download-cf.jetbrains.com/idea/ideaIC-2018.3.2.tar.gz
 tar -xvf ideaIC-2018.3.2.tar.gz
 
-cd ideaIC-2018.3.2
+cd ideaIC-2018.3.2/
 
 sudo ln -s ./ideaIC-2018.3.2/bin/idea.sh /usr/bin/idea
 
