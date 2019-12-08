2b9225898be9f09b6db79f8bfb8ddb9f2c6653b7
root <root@localhost.localdomain>
2019-12-08 20:31:02 +0100

    more function in files

--- fire_chro2.sh
+++ fire_chro2.sh
@@ -1,43 +1,51 @@
 #!/bin/bash
 
-while getopts ":h" opt;
-    do
-      	case $opt
+function CHROME {
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_x$
+        sudo yum localinstall google-chrome-stable_current_x86_64.rpm
+        echo "Chrome has been instaled"
+}
+
+function FIREFOX {
+        yum remove firefox
+        cd /usr/local
+        wget http://ftp.mozilla.org/pub/firefox/releases/70.0.1/linux-x86_64/e$
+        tar xvjf firefox-70.0.1.tar.bz2
+        sudo ln -s /usr/local/firefox/firefox /usr/bin/firefox
+        echo "FIrefox has been instaled"
+}
+
+
+while getopts ":hfc" opt;
+ do
+   	case $opt
         in
-            h) echo "if you need help ask your peer or Google"; exit 0;;
+            h) echo "Instal chrome -c install firefox -f "; exit 0;;
+            c) CHROME ; exit 2 ;;
+            f) FIREFOX ; exit 2 ;;
             ?) echo "value is not valid"; exit 2;;
         esac
  done
 
+
 echo "what  would you like to install: Chrome option one or Firefox option two or  both ?"
  
 # read -p INPUT_STRING
+
+# read -p INPUT_STRING
 INPUT_STRING=2
-	case $INPUT_STRING 
-	in
-        1)
-          	wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-                sudo yum localinstall google-chrome-stable_current_x86_64.rpm
-                echo "Chrome has been instaled"
-                ;;
-        2)
-          	yum remove firefox
-                cd /usr/local
-                wget http://ftp.mozilla.org/pub/firefox/releases/70.0.1/linux-x86_64/en-US/firefox-70.0.1.tar.b$
-                tar xvjf firefox-70.0.1.tar.bz2
-                sudo ln -s /usr/local/firefox/firefox /usr/bin/firefox
-                echo "FIrefox has been instaled"
-                ;;
-        *)
-          	echo "I have installed Firefox and Chrome"
-		wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-                sudo yum localinstall google-chrome-stable_current_x86_64.rpm
-		yum remove firefox
-                cd /usr/local
-                wget http://ftp.mozilla.org/pub/firefox/releases/70.0.1/linux-x86_64/en-US/firefox-70.0.1.tar.b$
-                tar xvjf firefox-70.0.1.tar.bz2
-                sudo ln -s /usr/local/firefox/firefox /usr/bin/firefox
-                ;;
+	case $INPUT_STRING
+        in
+	c)
+          CHROME
+          ;;
+	f)
+          FIREFOX
+          ;;
+	*)
+          FIREFOX
+          CHROME
+          ;;
   esac
 
 
--- idea_001.sh
+++ idea_001.sh
@@ -2,10 +2,27 @@
 
 sudo wget https://download-cf.jetbrains.com/idea/ideaIC-2018.3.2.tar.gz
 tar -xvf ideaIC-2018.3.2.tar.gz
-mkdir ideaIC-2018.3.2/
+
+
+if [ -d "ideaIC-2018.3.2/" ]; then
+   echo "Installing config files in ${DIR}..."
+   
+else
+  #echo "Error: ${DIR} not found. Can not continue."
+  mkdir ideaIC-2018.3.2/
+  exit 1
+fi
 
 cd ideaIC-2018.3.2/
-#rm idea 
+
+if [ -d "/usr/bin/idea" ]; then
+   rm /usr/bin/idea
+   echo " rm symb_link"
+   
+else
+  echo " create symb_link"
+fi
+
 sudo ln -s ./ideaIC-2018.3.2/bin/idea.sh /usr/bin/idea
 
 
